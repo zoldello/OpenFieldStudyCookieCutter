677fdfb857d2c7e22714a58902e6c7ae11415daa
Phil <zoldello@gmail.com>
2021-06-27 11:50:59 -0700

    Initial work (#2)

---
+++ setup.py
@@ -0,0 +1,29 @@
+# !/usr/bin/env python
+
+from distutils.core import setup
+setup(
+    name='open-study-experiments-demo',
+    packages=[],
+    version='0.1.0',
+    description='Cookiecutter template for a Python package for Open study experiments on mice',
+    author='Philip Adenekan',
+    license='MIT',
+    url='https://github.com/zoldello/OpenFieldStudyCookieCutter',
+    keywords=['cookiecutter', 'template', 'package', 'experiments', 'nwb', 'datajoint', 'neuroscience', 'mice', 'rat', 'setup', 'lab', 'laboratory', 'research', 'data', 'processing' ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+        'Topic :: Neuroscience Research',
+    ],
+)
